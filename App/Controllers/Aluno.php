ef3a03ebb6c12cdbe9ba87c84732cb0f243b5c1d
José Ramos <nettorammos@hotmail.com>
2019-11-12 14:20:44 -0300

    Incoporando validador de aluno

--- App/Controllers/Aluno.php
+++ App/Controllers/Aluno.php
@@ -3,16 +3,86 @@
      private $nome;
      private $email;
      private $ra;
-     private $cursos;
+     private $curso;
      private $cpf;
      private $dataNascimento ;
      private $contato;
      private $segundoContanto;
 
+    public $erros;
+
      function cadAluno (){
         
      }
-     public function validateStudent() {
-        
+     public function validateStudent($nome, $email, $curso, $cpf, $dataNascimento, $contato, $segundoContanto) {
+            $finalName = $this->validateNome($nome);
+            $finalEmail = $email;
+            $finalCurso = $curso;
+            $finalCpf = $this->validateCPF($cpf);
     }
+
+    public function validateNome($nome){
+        if(is_string($nome)){
+            return $nome;
+        }else {
+            $erros [] = [
+                "Nome Inválido"
+            ];
+        }
+    }
+    function validateCPF($cpf = null) {
+
+        if(empty($cpf)) {
+            $erros [] = [
+                "Insira o CPF"
+            ];
+        }
+    
+        // Elimina possivel mascara
+        $cpf = preg_replace("/[^0-9]/", "", $cpf);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+        
+        // Verifica se o numero de digitos informados é igual a 11 
+        if (strlen($cpf) != 11) {
+            $erros [] = [
+                "Insira um CPF válido"
+            ];
+        }
+        // Verifica se nenhuma das sequências invalidas abaixo 
+        // foi digitada. Caso afirmativo, retorna falso
+        else if ($cpf == '00000000000' || 
+            $cpf == '11111111111' || 
+            $cpf == '22222222222' || 
+            $cpf == '33333333333' || 
+            $cpf == '44444444444' || 
+            $cpf == '55555555555' || 
+            $cpf == '66666666666' || 
+            $cpf == '77777777777' || 
+            $cpf == '88888888888' || 
+            $cpf == '99999999999') {
+            $erros [] = [
+                "CPF inválido"
+            ];
+         // Calcula os digitos verificadores para verificar se o
+         // CPF é válido
+         } else {   
+            
+            for ($t = 9; $t < 11; $t++) {
+                
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf{$c} * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf{$c} != $d) {
+                    return false;
+                }
+            }
+    
+            return $cpf;
+        }
+    }
+    
+
+
+
  }
