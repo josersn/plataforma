e02379afc4591f45d8d72075e3d35682abddd192
José Ramos <nettorammos@hotmail.com>
2019-11-12 10:05:34 -0300

    :sparkles: Menu side e top

--- App/Views/dashboard.php
+++ App/Views/dashboard.php
@@ -9,44 +9,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="App/Views/dashboard/style.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="App/Views/header/style.css">
 
     <title>Área do aluno</title>
 </head>
 <body>
-    <aside>
-        <div class="profile">
-            <img src="App/Views/dashboard/img/profile.jpeg" alt="José Ramos">
-            <h3>José Ramos</h3>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="#">Dashboard</a></li>
-                <li><a href="#">Aulas</a></li>
-                <li><a href="#">Notas</a></li>
-                <li><a href="#">Biblioteca</a></li>
-                <li><a href="#">Suporte</a></li>
-                <li><a href="#">Financeiro</a></li>
-                <li><a href="#">Sair</a></li>
-            </ul>
-        </nav>
-        <h1>Saphir Educ - EAD Plataforma</h1>
-    </aside>
-    <main>
-        <i class="fas fa-bars fa-2x me-2 border"></i>
-        <div class="row col-40">
-            <section class="mail">
-                <div class="mail-title">
-                    
-                <h3>Caixa de mensagens</h3>
-                </div>
-                <div class="mail-body">
-
-                </div>
-            </section>
-            <section class="maperil"></section>
-        </div>
-        <div class="row"></div>
-    </main>
+    <?php include('App/Views/header.php')?>
+        
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("nav ul img").click(function () {
+                $(".close").slideToggle();
+            })
+        })
+    </script>
 </body>
 </html>
---
+++ App/Views/header.php
@@ -0,0 +1,32 @@
+<section class="menu-side close">
+    <div class="profile">
+        <img src="App\Views\dashboard\img\profile.jpeg" alt="Imagem Perfil">
+        <h2>José Ramos</h2>
+        <h3>Gestão Comercial</h3>
+    </div>
+    <nav class="nav-side">
+        <ul>
+            <li><a href="#">Notas e Faltas</a></li>
+            <li><a href="#">Secretária Online</a></li>
+            <li><a href="#">Biblioteca Virtual</a></li>
+            <li><a href="#">Perfil</a></li>
+            <li><a href="#">Sair</a></li>
+        </ul>
+    </nav>
+</section>
+<header>
+    <div class="logo">
+    <h1>Saphir<span>Educ</span> EAD</h1>
+    </div>
+    <nav class="menu-top">
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Aulas</a></li>
+            <li><a href="#">José Ramos</a></li>
+            <img src="App/Views/header/img/bars-solid.svg" alt="Menu">
+        </ul>
+    </nav>
+    
+</header>
+
+    
